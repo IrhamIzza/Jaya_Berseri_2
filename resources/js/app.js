bb7cffdc41cd827694628c15948f3323b72709eb
IrhamIzza <Irhamizza21@gmail.com>
2025-02-04 00:00:01 +0700

    Add js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 import './bootstrap';
 
 let hidden
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
