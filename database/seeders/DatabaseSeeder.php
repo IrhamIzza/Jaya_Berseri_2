e2dd3e749ba8390e6e80e1468a82100e69bab25f
IrhamIzza <Irhamizza21@gmail.com>
2025-02-07 23:03:02 +0700

    add seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +18,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+        [    'name' => "irham",
+            'email' => Str::random(10).'@example.com',
+            'password' => Hash::make('123'),
+        ],
+        [    'name' => "kucing",
+            'email' => Str::random(10).'@example.com',
+            'password' => Hash::make('123'),
+        ],
+            
         ]);
     }
 }
